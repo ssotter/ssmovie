2b7892cd64c4bf44138c5015a58dcce157b9fcc1
Sergio Llopart Sotter <sergio.sotter@network-rg.com.br>
2022-03-08 17:07:51 -0300

    Project created

---
+++ backend/src/main/java/com/ssotter/ssmovie/SsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.ssotter.ssmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssotter/ssmovie/SsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssotter.ssmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
